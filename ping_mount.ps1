ddf221aab17bafb5afb1812312d044c70ce77567
Jacob/Malus <Jkirstejn@gmail.com>
2019-11-19 16:30:37 +0100

    AZ Powershell notation to CLI
    Couldn't use AZ Powershell commands, even though I installed the packages and logged in, using a service principal account.
    I had to log in using the AZ Powershell command, which will always prompt the user for username and password, or request a certificate file, which I will try to avoid using - because I'm an stubborn idiot.

--- ping_mount.ps1
+++ ping_mount.ps1
@@ -1,18 +1,52 @@
-Install-Module -Name Az -AllowClobber -Scope CurrentUser
+# Install dependencies and azure cmdlets
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module -Name Az -AllowClobber -Scope CurrentUser -Force
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
+
+# Restart powershell session after installs
+powershell
+
+# Login with my service principal credentials (CLI)
+az login --service-principal -u args[0] -p args[1] --tenant args[2]
 
 $resourceGroupName = "case2rg"
-$storageAccountName = (Get-AzResource -ResourceGroupName $resourceGroupName -Name case2sac*).Name
+$storageAccountName = args[3]
 
-$storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName
-$storageAccountKeys = Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName
+$storageAccountKeys = az storage account keys list --account-name $storageAccountName --query '[0].value' -o tsv
 
-Invoke-Expression -Command ("cmdkey /add:$([System.Uri]::new($storageAccount.Context.FileEndPoint).Host) " + `
-    "/user:AZURE\$($storageAccount.StorageAccountName) /pass:$($storageAccountKeys[0].Value)")
+Invoke-Expression -Command ("cmdkey /add: $($storageAccountName)" + ".file.core.windows.net " + `
+    "/user:AZURE\$($storageAccountName) /pass:$($storageAccountKeys)")
 
-Invoke-Expression -Command ("net use Z: \\$([System.Uri]::new($storageAccount.Context.FileEndPoint).Host)" + `
+Invoke-Expression -Command ("net use Z: \\$($storageAccountName)" + ".file.core.windows.net\fileshareping " + `
     "\fileshareping /persistent:Yes")
-    
+
 # Change directory, ping and save result in log
-$vm2ip = (Get-AzPublicIpAddress -ResourceGroupName "case2rg" -Name vm2-ip).IpAddress
+$vm2ip = az network public-ip list --query "[1].ipAddress"
 Z:
 ping $vm2ip > pinglog.txt
+
+
+################################################
+# AZ POWERSHELL implementation
+#
+# Failed attempt to log in using powershell
+# $username = args[4]
+# $password =ConvertTo-SecureString args[5] -AsPlainText -Force
+# $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $password
+#
+#$storageAccountName = (Get-AzResource -ResourceGroupName $resourceGroupName -Name case2sac*).Name
+#
+#$storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName
+#$storageAccountKeys = Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName
+#
+#Invoke-Expression -Command ("cmdkey /add:$([System.Uri]::new($storageAccount.Context.FileEndPoint).Host) " + `
+#    "/user:AZURE\$($storageAccount.StorageAccountName) /pass:$($storageAccountKeys[0].Value)")
+#Invoke-Expression -Command ("net use Z: \\$([System.Uri]::new($storageAccount.Context.FileEndPoint).Host)" + `
+#    "\fileshareping /persistent:Yes")
+#
+# $vm2ip = (Get-AzPublicIpAddress -ResourceGroupName "case2rg" -Name vm2-ip).IpAddress
+#
+# Z:
+# ping $vm2ip > pinglog.txt
+#
+###############################################
